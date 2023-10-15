44b047bc6d3fb633ed31f1736e951137d7a96037
mohnishaggarwal <mohnisha@umich.edu>
2023-10-15 19:23:31 -0400

    back to normal

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -18,28 +18,16 @@ function usePrevious(value) {
   return ref.current
 }
 
-function BunAppreciationDay() {
-  return (
-    <div className='w-full h-screen flex flex-col justify-center items-center bg-purple-500 font-bold'>
-      <Image src={Boun} className="object-cover w-48 rounded-3xl" />
-      <h1 className='text-4xl mt-16'>
-        Happy Bun Appreciation Day Meri Jaan! ❤️
-      </h1>
-    </div>
-  );
-}
-
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
 
   return (
     <div className="relative">
-      {/* <Header />
+      <Header />
       <main>
         <Component previousPathname={previousPathname} {...pageProps} />
       </main>
-      <Footer /> */}
-      <BunAppreciationDay />
+      <Footer />
     </div>
   )
 }
